aa267db0585348b9fb0957b0e606fb9317cd720a
mrsiddhantpro67-source <mrsiddhantpro67@gmail.com>
2026-09-22 18:24:42 +0530

    server.js

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+// Process.env.PORT Render ke liye zaroori hai
 const PORT = process.env.PORT || 3000;
 
-// 'public' folder se static files serve karne ke liye
+// Public folder se index.html serve karna
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Root route - index.html render karega
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Server start karein
 app.listen(PORT, () => {
-    console.log(`☁️ CloudDrive Server running at: http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
